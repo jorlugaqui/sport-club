5567a3c0f86b10403520ac50c711f5679ee31986
Jorge Luis Galvis Quintero <jorge.galvis@mercadolibre.com.co>
2023-04-10 18:57:38 -0500

    Add Club model

--- schools/models.py
+++ schools/models.py
@@ -6,3 +6,13 @@ class Sport(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+class Club(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    address = models.CharField(max_length=255)
+    director = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=25)
+    sport = models.ForeignKey(Sport, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.director}" 
