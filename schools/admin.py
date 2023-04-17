06697ad29d66e710c9f5348faae619990d3172ef
BenZ2PG <65003007+BenZ2PG@users.noreply.github.com>
2023-04-17 18:46:11 -0500

    Add period model

--- schools/admin.py
+++ schools/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Sport, Club
+from .models import Sport, Club, Period
 
 
 admin.site.register(Sport)
 admin.site.register(Club)
+admin.site.register(Period)
