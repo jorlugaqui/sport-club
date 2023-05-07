3fbeb56378fdfa1b3ad8f729d4099d8a79d65484
BenZ2PG <65003007+BenZ2PG@users.noreply.github.com>
2023-05-07 11:24:37 -0500

    Add personalized admin

--- schools/admin.py
+++ schools/admin.py
@@ -2,8 +2,25 @@ from django.contrib import admin
 from .models import Sport, Club, Period, Facility, Coach
 
 
-admin.site.register(Sport)
-admin.site.register(Club)
-admin.site.register(Period)
-admin.site.register(Facility)
-admin.site.register(Coach)
+@admin.register(Sport)
+class SportAdmin(admin.ModelAdmin):
+    ordering = ('name',)
+
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    ordering = ('name', 'director')
+    list_display = ('name', 'director')
+
+@admin.register(Period)
+class PeriodAdmin(admin.ModelAdmin):
+    ordering = ('name', 'start_date', 'end_date')
+    list_display = ('name', 'start_date', 'end_date')
+
+@admin.register(Facility)
+class FacilityAdmin(admin.ModelAdmin):
+    list_display = ('name','availability')
+
+@admin.register(Coach)
+class CoachAdmin(admin.ModelAdmin):
+    list_display = ('name','phonenumber')
