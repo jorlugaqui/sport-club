5284d20068f86791c51aa7df50a157ae495a283c
BenZ2PG <65003007+BenZ2PG@users.noreply.github.com>
2023-05-14 23:34:55 -0500

    Deportist model was created

--- deportist/admin.py
+++ deportist/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Deportist
 
-# Register your models here.
+@admin.register(Deportist)
+class DeportistAdmin(admin.ModelAdmin):
+    ordering = ('lastname',)
--- deportist/models.py
+++ deportist/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Deportist(models.Model):
+    registrationcode = models.IntegerField(max_length=3)
+    name = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    phonenumber = models.CharField(max_length=60)
+    birthdate = models.DateField()
+    dni = models.CharField(max_length=255)
