4f795cc5a544bdb7ebbd19cd16eca3c32d269b9b
BenZ2PG <65003007+BenZ2PG@users.noreply.github.com>
2023-05-15 21:17:39 -0500

    the code field has been modified

--- deportist/models.py
+++ deportist/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from schools.models import Club
 # Create your models here.
 class Deportist(models.Model):
-    code = models.IntegerField()
+    code = models.IntegerField(editable=False)
     name = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
     phonenumber = models.CharField(max_length=60)
@@ -12,6 +12,9 @@ class Deportist(models.Model):
 
     def save(self, *args, **kwargs):
         self.code = f"{self.club_id.id}{self.code}"
+        if not self.id:
+            highest_code = Deportist.objects.aggregate(models.Max('code'))['code__max']
+            self.code = highest_code + 1 if highest_code is not None else 1
         super().save(*args, **kwargs)
 
     def __str__(self) -> str:
