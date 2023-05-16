1eb539bf561e9d2f6b9ca5ed8bf6cdfb1933fae6
BenZ2PG <65003007+BenZ2PG@users.noreply.github.com>
2023-05-16 17:18:39 -0500

    all corrections done, except the on_update

--- deportist/models.py
+++ deportist/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from schools.models import Club
+
 # Create your models here.
+
 class Deportist(models.Model):
     code = models.IntegerField(editable=False)
     name = models.CharField(max_length=255)
@@ -8,14 +10,26 @@ class Deportist(models.Model):
     phonenumber = models.CharField(max_length=60)
     birthdate = models.DateField()
     dni = models.CharField(max_length=255)
-    club_id = models.ForeignKey(Club, null=True, on_delete=models.CASCADE)
+    club = models.ForeignKey(Club, null=True, on_delete=models.RESTRICT)
 
     def save(self, *args, **kwargs):
-        self.code = f"{self.club_id.id}{self.code}"
         if not self.id:
-            highest_code = Deportist.objects.aggregate(models.Max('code'))['code__max']
-            self.code = highest_code + 1 if highest_code is not None else 1
-        super().save(*args, **kwargs)
+            existing_records = Deportist.objects.filter(club=self.club)
+            if existing_records.exists():
+                self.code = f"{self.club.id}{self.code}"
+                highest_code = Deportist.objects.filter(club=self.club).aggregate(models.Max('code'))['code__max']
+                self.code = highest_code + 1 if highest_code is not None else 1
+                super().save(*args, **kwargs)
+            else:
+
+                highest_code = Deportist.objects.filter(club=self.club).aggregate(models.Max('code'))['code__max']
+                self.code = highest_code + 1 if highest_code is not None else 1
+                self.code = f"{self.club.id}{self.code}"
+                super().save(*args, **kwargs)
+        else:
+            super().save(*args, **kwargs)
+        
+
 
     def __str__(self) -> str:
         return f"{self.code} - {self.name} - {self.lastname}" 
