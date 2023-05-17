0b210b616c7f82a38469b031a51a5479dfaf8b61
BenZ2PG <65003007+BenZ2PG@users.noreply.github.com>
2023-05-16 19:45:27 -0500

    corrections done

--- deportist/models.py
+++ deportist/models.py
@@ -12,26 +12,25 @@ class Deportist(models.Model):
     dni = models.CharField(max_length=255)
     club = models.ForeignKey(Club, null=True, on_delete=models.RESTRICT)
 
-    
     def save(self, *args, **kwargs):
         if not self.id:
-            existing_records = Deportist.objects.filter(club=self.club).order_by('-code')[:1]
+            existing_records = Deportist.objects.filter(club=self.club).order_by('-code')[:1] #se filtra y se limita a 1
             if existing_records.exists():
                 existing_code = existing_records[0].code
-                if existing_code != self.id:
-                    self.code = str(existing_code + 1).zfill(5)
+                if existing_code != self.id: #se compara si es diferente de self.id
+                    self.code = str(existing_code + 1).zfill(5) #al ser diferente se le suma 1 al código y guarda
                     super().save(*args, **kwargs)
-                else:
-                    if existing_records[0].club != self.club:
+                else: #si son diferentes
+                    if existing_records[0].club != self.club: #se valida el club
                         highest_code = Deportist.objects.filter(club=self.club).aggregate(models.Max('code'))['code__max']
                         self.code = str(highest_code + 1).zfill(5) if highest_code is not None else '00001'
                         super().save(*args, **kwargs)
-            else:
+            else: #si no existe ningún dato guardado
                 highest_code = Deportist.objects.filter(club=self.club).aggregate(models.Max('code'))['code__max']
                 self.code = str(highest_code + 1).zfill(5) if highest_code is not None else '00001'
                 self.code = f"{self.club.id}{self.code}"
                 super().save(*args, **kwargs)
-        else:
+        else: #si no es ninguna de las anteriores no se hace nada
             super().save(*args, **kwargs)
 
     def __str__(self) -> str:
